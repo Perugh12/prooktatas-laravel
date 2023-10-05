c34e7fa2e518adf692de4ce0c348deaaee28cdc8
Perugh12 <perugh12@gmail.com>
2023-10-05 19:39:57 +0200

    feat: laravel-controller

---
+++ laravel-ecommerce/app/Http/Controllers/ProductController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function list()
+    {
+        // Ki lehet vele logolni a lekérdezéseket
+        // \DB::enableQueryLog();
+        // dd(\DB::getQueryLog());
+
+        # Adatbázis lekérés, lekéri a termékeket
+        $products = Product::where('stock', '>', 0)->get();
+
+        return view('products.index', [
+            'products' => $products
+        ]);
+    }
+}
